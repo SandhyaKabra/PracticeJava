b0776b075c853433458ae5426abce89d64f3bd95
Ashish Kumar Lahoti <lahoti.ashish20@gmail.com>
2022-07-14 23:55:48 +0800

    All changes

--- src/com/company/RomanToInteger.java
+++ src/com/company/RomanToInteger.java
@@ -14,7 +14,37 @@ package com.company;
  */
 public class RomanToInteger {
 
-    public int convertToInteger(String roman){
-        return 0;
+    public static void main(String args[]) {
+        RomanToInteger Callobj = new RomanToInteger();
+        Callobj.convertToInteger("IXIV");
+    }
+
+    public static void convertToInteger(String roman) {
+        for (int i = 0; i <roman.length() ; i++) {
+            if (roman.charAt(i) != roman.charAt(i+1))
+            {
+                
+            }
+        }
+    }
+
+    public static void totalsum(char s) {
+        int value = 0, value1 = 0;
+        if (s == 'I') {
+            value = 1;
+            System.out.println("I is " + value);
+        }
+        if (s == 'V') {
+            value = 5;
+            System.out.println("V is " + value);
+        }
+        if (s == 'X') {
+            value = 10;
+            System.out.println("X is " + value);
+        }
+        value1 = value1 + value;
     }
 }
+
+
+
